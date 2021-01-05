619f145797dabaa5f60ab154e9bef082e93f8b27
Charlie <peabody00@gmail.com>
2021-01-05 14:22:59 -0600

    Random Mad Lib Generation Basic code made to randomly fill out the entire Mad Lib.  Still need solution from API developer about being able to find more specific random words.

--- lib/madlib1.rb
+++ lib/madlib1.rb
@@ -4,7 +4,6 @@ class Madlib1
     def initialize
         @word_list = ["adjective", "noun"]
         @new_list = []
-        # create_list
         random_madlib
     end
 
@@ -13,7 +12,12 @@ class Madlib1
         puts "Would you like to randomly generate the words for this Mad Lib? (y/n)"
         input = gets.strip.downcase
         if input == "y"
-            puts "random word list generated"
+            # This will connect to API Class to randomly generate the entire list of words for the Mad Lib.  Still needs validation.
+            # If @word_list element is a noun it needs to randomly find a noun.
+            @word_list.each do |n| 
+                @new_list << API.new("random").random_word
+            end
+            complete_madlib
         elsif input == "n"
             create_list
         else
@@ -22,21 +26,23 @@ class Madlib1
         end
     end
 
-    def random_madlib_generator
-        
-    end
-
-    
     def create_list
         input = ""
         @word_list.each_with_index do |word, index|
             puts "Enter a(n) #{word.green} or type 'random' for a random #{word.green}"
             input = gets.strip.downcase
                 if input != "random"
+                    #I want to add validation to this step so program can look up word entered and make sure it matches the part of speech being asked for
+                    
+                    # In progress code
+                    # valid = API.new(input).valid_partofspeech?
+                    # while valid == false
+                    #     puts "I am sorry.  Your word appears to not be a(n) #{word.green}.  Please try again."
+                    #     input = gets.strip.downcase
+                    
                     @new_list << input
                 else
                     #This will connect to the API class to search for a random word that matches the word type and then shovels it into @new_list
-                    # @new_list << "random"
                     @new_list << API.new("random").random_word
                 end
         end
@@ -50,12 +56,11 @@ class Madlib1
     end
 
     def return_to_menu
-        input = ""
         puts ""
         puts "---------------------------------------"
         puts "Type 'menu' to return to the main menu."  # Want to add this functionality "Otherwise type 'exit' to quit program."
         input = gets.strip.downcase
-        if input == "menu"
+        if input == "menu" #this creates a new CLI instance.  Need to fix that.  requires typing exit twice to exit.
             CLI.new.call
         # elsif input == "exit"
         #     CLI.new.exit_program
